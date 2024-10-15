3455c146b4beea1e23bf8f42ca1a315a766a4386
monptt <ptt_kr@outlook.jp>
2024-10-15 23:29:42 +0900

    スペース区切り

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,13 +1,25 @@
 mod tokenizer;
+use std::collections::HashMap;
 
 pub fn interpret(code: &str) -> String {
     let mut output = String::from("");
     
+    // 宣言された変数を格納するハッシュマップ
+    let mut variables: HashMap<&str, i32> = std::collections::HashMap::new();
+
     for line in code.lines(){
         // ここで行ごとに処理する
-        let _token_list = tokenizer::TokenList::new(line);
+        let tokens = tokenizer::TokenList::new(line).tokens;
 
-        for token in _token_list.tokens{
+        if tokens[0] == "let"{
+
+        }
+        else if tokens[0] == "eval"{
+            
+        }
+
+
+        for token in tokens{
             output.push_str(token);
             output.push_str("\n");
         }
