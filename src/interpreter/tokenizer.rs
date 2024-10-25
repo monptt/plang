c133d14c37502154058147dc768a7bf597200163
monptt <ptt_kr@outlook.jp>
2024-10-26 02:44:43 +0900

    スペースなくても記号をトークン化

--- src/interpreter/tokenizer.rs
+++ src/interpreter/tokenizer.rs
@@ -3,20 +3,20 @@ use std::fmt;
 use crate::object::number::integer::Integer;
 
 #[derive(Clone)]
-pub struct Token{
-    word: String
+pub struct Token {
+    word: String,
 }
 
 impl Token {
-    pub fn new(word: String) -> Token{
-        return Token{word: word};
+    pub fn new(word: &String) -> Token {
+        return Token { word: word.clone() };
     }
 
-    pub fn get_word(&self) -> &String{
+    pub fn get_word(&self) -> &String {
         return &self.word;
     }
 
-    pub fn to_integer(&self) -> Integer{
+    pub fn to_integer(&self) -> Integer {
         return Integer::new(&self.get_word());
     }
 }
@@ -27,30 +27,52 @@ impl fmt::Display for Token {
     }
 }
 
-pub struct TokenList{
-    tokens : Vec<Token>
+pub struct TokenList {
+    tokens: Vec<Token>,
 }
 
-impl TokenList{
-    pub fn new(line : &String) -> TokenList{
-        let mut token_list = TokenList{tokens: Vec::new()};
+impl TokenList {
+    pub fn new(line: &String) -> TokenList {
+        let mut token_list = TokenList { tokens: Vec::new() };
 
         // とりあえずスペースで区切る
         let words: Vec<_> = line.split(" ").collect();
 
-        for word in words{
-            let token = Token::new(word.to_string());
-            token_list.tokens.push(token);
+        for word in words {
+            let mut temp_token = String::from("");
+            for c in word.chars() {
+                if "=+-*/[],".contains(c) {
+                    // 記号が来たらその時点でトークン化
+                    if temp_token.len() > 0 {
+                        let token = Token::new(&temp_token);
+                        token_list.tokens.push(token);
+                    }
+
+                    // 記号もトークン化
+                    token_list.tokens.push(Token::new(&c.to_string()));
+                    // 文字列をリセット
+                    temp_token = String::from("");
+                } else {
+                    // それ以外は文字列に突っ込む
+                    temp_token.push_str(c.to_string().as_str());
+                }
+            }
+
+            // 残ってたらトークン化
+            if temp_token.len() > 0 {
+                let token = Token::new(&temp_token);
+                token_list.tokens.push(token);
+            }
         }
 
         return token_list;
     }
 
-    pub fn get_token(&self, index: usize) -> &Token{
+    pub fn get_token(&self, index: usize) -> &Token {
         return &self.tokens.get(index).unwrap();
     }
 
-    pub fn get_vec(&self) -> &Vec<Token>{
+    pub fn get_vec(&self) -> &Vec<Token> {
         return &self.tokens;
     }
 }
