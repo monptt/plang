6224dfa1fdff1480823606d646ac8c9eb7266bc1
monptt <ptt_kr@outlook.jp>
2024-11-03 00:10:55 +0900

    写像トレイト

--- src/object/function/function.rs
+++ src/object/function/function.rs
@@ -1,5 +1,9 @@
-use crate::object::number::rational_number::RationalNumber;
+use crate::object::{morphism::Morphism, number::rational_number::RationalNumber};
 
 pub trait Function {
     fn map(&self, x: RationalNumber) -> RationalNumber;
+}
+
+impl Morphism for dyn Function {
+    
 }
--- src/object/morphism.rs
+++ src/object/morphism.rs
@@ -1,6 +1,9 @@
 use super::object::ObjectTrait;
 
-pub struct Morphism<'a> {
-    initial_object: &'a dyn ObjectTrait,
-    final_object: &'a dyn ObjectTrait
+// pub struct Morphism<'a> {
+//     initial_object: &'a dyn ObjectTrait,
+//     final_object: &'a dyn ObjectTrait
+// }
+
+pub trait Morphism {
 }
